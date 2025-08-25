bec161f431402e9dc26f242aacd1cb378ea717e3
QuocLam <developers.dion@gmail.com>
2025-08-25 21:46:24 +0700

    add

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'controllers/auth_controller.dart';
+import 'services/auth_repository.dart';
+import 'routes.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AuthRepository.init();                     // init storage/session
+  final auth = AuthController(AuthRepository());   // tạo controller
+  runApp(MyApp(auth: auth));                       // truyền vào MyApp (KHÔNG const)
+}
+
+class MyApp extends StatelessWidget {
+  final AuthController auth;
+  const MyApp({super.key, required this.auth});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'App',
+      theme: ThemeData(useMaterial3: true),
+      initialRoute: '/',                                       // Splash -> /login|/home
+      onGenerateRoute: (settings) => onGenerateRoute(settings, auth),
+      onUnknownRoute: (_) => MaterialPageRoute(
+        builder: (_) => const Scaffold(
+          body: Center(child: Text('Route not found')),
+        ),
+      ),
+    );
+  }
+}

1d47126a38e327d63cf7263662ab1756a046174b
lam.nguyenquoc1 <lam.nguyenquoc1@V004406>
2025-09-09 17:12:47 +0700

    updatte firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -17,6 +19,10 @@ android {
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+    
+    dependencies {
+        implementation(platform("com.google.firebase:firebase-bom:34.2.0"))
     }
 
     defaultConfig {
